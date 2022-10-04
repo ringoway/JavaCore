b18af59b1c98417ef86d6239ba695aca84d12b7a
Azat <azat.takhautdinov@gmail.com>
2022-10-04 21:39:47 +0500

    Generics

---
+++ src/Generics/Main.java
@@ -0,0 +1,14 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("hi");
+        list.add(303);
+        System.out.println(list);
+    }
+}
+
