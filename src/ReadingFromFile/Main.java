254d143f2b8d494fffcb46b7d1acd05c7f44cd98
Azat <azat.takhautdinov@gmail.com>
2022-10-14 21:56:36 +0500

    Reading from file start

---
+++ src/ReadingFromFile/Main.java
@@ -0,0 +1,11 @@
+package ReadingFromFile;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
