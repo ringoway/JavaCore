a34b6f41ff1e338c041f0f4289a5926029471dd5
Azat <azat.takhautdinov@gmail.com>
2022-10-14 22:18:49 +0500

    Reading from file

--- src/ReadingFromFile/Main.java
+++ src/ReadingFromFile/Main.java
@@ -1,11 +1,20 @@
 package ReadingFromFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input);
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = separator+"Users"+separator+"azat"+separator+"Desktop"+separator+"ReadingFromFile.txt";
+
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
     }
 }
