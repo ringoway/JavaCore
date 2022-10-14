5485a3f85c2a2787a79942b3a706b679699cc034
Azat <azat.takhautdinov@gmail.com>
2022-10-15 01:25:32 +0500

    update exception

--- src/Exceptions/CreateException/Main.java
+++ src/Exceptions/CreateException/Main.java
@@ -1,20 +1,14 @@
 package Exceptions.CreateException;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScannerException {
         Scanner scanner = new Scanner(System.in);
         while(true) {
             int x = Integer.parseInt(scanner.nextLine());
 
             if(x != 0) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                throw new ScannerException("Вы написали что-то кроме 0");
             }
         }
     }
---
+++ src/Exceptions/CreateException/ScannerException.java
@@ -0,0 +1,7 @@
+package Exceptions.CreateException;
+
+public class ScannerException extends Exception{
+    public ScannerException(String description){
+        super(description);
+    }
+}
