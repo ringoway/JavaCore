10014c9a4bb4f18a619b864c10026990ff1f22ab
Azat <azat.takhautdinov@gmail.com>
2022-10-15 01:47:13 +0500

    other

---
+++ src/Exceptions/OtherException/Main5.java
@@ -0,0 +1,21 @@
+package Exceptions.OtherException;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Main5 {
+    public static void main(String[] args) {
+        try {
+            run();
+        } catch (IOException | IllegalAccessException | ParseException e) { //multi catch
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public static void run()throws IOException, ParseException, IllegalAccessException {
+
+    }
+}
