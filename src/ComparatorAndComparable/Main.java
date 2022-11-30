18533a0172818a70007802a8d7b5d3b84f918f5f
Azat <azat.takhautdinov@gmail.com>
2022-11-30 22:54:01 +0500

    update, + name

--- src/ComparatorAndComparable/Main.java
+++ src/ComparatorAndComparable/Main.java
@@ -8,8 +8,10 @@ public class Main{
     public static void main(String[] args) {
         List<Message> messages = new ArrayList<>();
 
-        messages.add(new Message("Hello world"));
-        messages.add(new Message("Hello countries"));
+        messages.add(new Message("Nikola","Hello world"));
+        messages.add(new Message("Mickael","Hello countries"));
+        messages.add(new Message("Robert","Hello city"));
+
 
         for (Message message: messages) {
             System.out.println(message);
